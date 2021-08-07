c5512c11f62707a853160035c13799017a6cb7b7
Cypherpunk Samurai <66906402+CypherpunkSamurai@users.noreply.github.com>
2021-08-07 21:18:59 +0530

    fix cookies required
    fix cookies required for some links

--- bypass-oracle-signup.js
+++ bypass-oracle-signup.js
@@ -1,20 +1,17 @@
+// ==UserScript==
+// @name        Oracle Signup Bypass
+// @namespace   Violentmonkey Scripts
+// @match       https://*.oracle.com/*
+// @grant       none
+// @license      MIT
+// @version     4.0
+// @author      - CypherpunkSamurai
+// @description        Download from Oracle videos without signup.
+// ==/UserScript==
 
+// This removes the disabled-class from the download links
+function remove_disable(e) {
+  e.setAttribute("href", e.getAttribute("data-file").replace("download.oracle.com", "edelivery.oracle.com").repace("/otn/", "/otn-pub/"));
+}
 
-    // ==UserScript==
-    // @name        Oracle Signup Bypass
-    // @namespace   Violentmonkey Scripts
-    // @match       https://*.oracle.com/*
-    // @grant       none
-    // @license      MIT
-    // @version     3.0
-    // @author      - CypherpunkSamurai
-    // @description        Download from Oracle videos without signup.
-    // ==/UserScript==
-     
-    // This removes the disabled-class from the download links
-    function remove_disable(e) {
-      e.setAttribute("href", e.getAttribute("data-file").replace("download.oracle.com", "edelivery.oracle.com"));
-    }
-     
-    document.querySelectorAll(".otable-w2 .license-link").forEach(remove_disable);
-
+document.querySelectorAll(".otable-w2 .license-link").forEach(remove_disable);
