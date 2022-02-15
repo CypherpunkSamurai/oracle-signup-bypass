cdfb0b095591c78b9067aa8241055506e374dc41
Martin W. Kirst <maki@bitkings.de>
2022-02-15 09:34:32 +0100

    fix linter issues

--- bypass-oracle-signup.js
+++ bypass-oracle-signup.js
@@ -37,7 +37,7 @@ function setCookie(name,value,days, domain) {
         expires = "; expires=" + date.toUTCString();
     }
     // set cookie
-    document.cookie = name + "=" + (value || "")  + expires + ";domain=" + domain + ";path=/";
+    document.cookie = name + "=" + (value || "") + expires + ";domain=" + domain + ";path=/";
 }
 
 
@@ -47,7 +47,7 @@ function setCookie(name,value,days, domain) {
 function make_invisible() {
   var htmlDiv = document.createElement('div');
   htmlDiv.innerHTML = "<style>.w11w2{background-color: rgb(255, 255, 255);max-width: 1092px;visibility: hidden;}</style>";
-  document.getElementsByTagName('head')[0].appendChild(htmlDiv); 
+  document.getElementsByTagName('head')[0].appendChild(htmlDiv);
 }
 
 
@@ -65,17 +65,17 @@ window.addEventListener('load', function() {
     // Set cookies for .oracle.com domain (including CDN). #fix_issue #1
     setCookie('oraclelicense','141',7, '.oracle.com');
     setCookie('AKA_A2','A',7, '.oracle.com');
-    
+
     // Replace Download Links
     alldownloads.forEach(assign_links);
-    
+
     // Make the license window invisible
     make_invisible();
-  
+
     // Alert User About Bypass.... (disabled default)
     //alert("All Download Links Have Been Unlocked.... Thank You for Using the UserScript - CypherpunkSamurai");
 }, false);
 
 // Thanks for Using the Script :)
-// 
+//
 // - CypherpunkSamurai (https://github.com/CypherpunkSamurai)
